d61e2e989955a155b7cabb0752b44f46821387b8
muriplz <46825658+muriplz@users.noreply.github.com>
2024-12-11 13:27:47 +0100

    Changed figura button with wardrobe button

--- common/src/main/java/org/figuramc/figura/mixin/gui/PauseScreenMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/gui/PauseScreenMixin.java
@@ -24,94 +24,26 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PauseScreen.class)
 public class PauseScreenMixin extends Screen {
-
+    @Unique
+    private static final ResourceLocation KRYEIT_ICON = new FiguraIdentifier("textures/gui/kryeit.png");
     protected PauseScreenMixin(Component title) {
         super(title);
     }
 
-    @Unique
-    private static final ResourceLocation FIGURA_ICON = new FiguraIdentifier("textures/gui/icon.png");
-
-    @Unique
-    private LayoutElement lanButton;
-
-    @Inject(method = "createPauseMenu",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;isLocalServer()Z"),
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;hasSingleplayerServer()Z"),
-                    to = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;isLocalServer()Z")
-            ),
-            locals = LocalCapture.CAPTURE_FAILSOFT
-    )
-    private void saveLanButton(CallbackInfo ci, GridLayout gridLayout, GridLayout.RowHelper rowHelper) {
-        gridLayout.visitChildren(element -> lanButton = element);
-    }
-
     @Inject(method = "createPauseMenu", at = @At("RETURN"))
-    private void createPauseMenuButton(CallbackInfo ci) {
-        int x, y;
+    private void createTopButton(CallbackInfo ci) {
+        int optionsButtonX = this.width / 2 - 100;
+        int optionsButtonY = this.height / 4 + 81;
 
-        int config = Configs.BUTTON_LOCATION.value;
-        switch (config) {
-            case 1 -> { // top left
-                x = 4;
-                y = 4;
-            }
-            case 2 -> {// top right
-                x = this.width - 68;
-                y = 4;
-            }
-            case 3 -> { // bottom left
-                x = 4;
-                y = this.height - 24;
-            }
-            case 4 -> { // bottom right
-                x = this.width - 68;
-                y = this.height - 24;
-            }
-            default -> { // icon
-                x = lanButton == null ? this.width / 2 + 106 : lanButton.getX() + lanButton.getWidth() + 4;
-                y = lanButton == null ? this.height / 4 + 80 : lanButton.getY();
-            }
-        }
+        int x = optionsButtonX - 25;
+        int y = optionsButtonY;
 
-        if (config > 0) { // button
-            addRenderableWidget(new Button(x, y, 64, 20, FiguraText.of(), null, btn -> this.minecraft.setScreen(new WardrobeScreen(this))) {
-                @Override
-                public void renderWidget(GuiGraphics gui, int mouseX, int mouseY, float delta) {
-                    ChatFormatting color;
-                    if (this.isHoveredOrFocused()) {
-                        color = ChatFormatting.AQUA;
-                    } else if (AvatarManager.panic) {
-                        color = ChatFormatting.GRAY;
-                    } else {
-                        color = ChatFormatting.WHITE;
-                    }
-                    setMessage(getMessage().copy().withStyle(color));
-
-                    renderVanillaBackground(gui, mouseX, mouseY, delta);
-                    super.renderWidget(gui, mouseX, mouseY, delta);
-                }
-
-                @Override
-                protected void renderDefaultTexture(GuiGraphics gui, float delta) {}
-            });
-        } else { // icon
-            addRenderableWidget(new Button(x, y, 20, 20, 0, 0, 20, FIGURA_ICON, 60, 20, null, btn -> this.minecraft.setScreen(new WardrobeScreen(this))) {
-                @Override
-                public void renderWidget(GuiGraphics gui, int mouseX, int mouseY, float delta) {
-                    renderVanillaBackground(gui, mouseX, mouseY, delta);
-                    super.renderWidget(gui, mouseX, mouseY, delta);
-                }
-
-                @Override
-                protected int getU() {
-                    int u = super.getU();
-                    if (u == 1 && AvatarManager.panic)
-                        return 0;
-                    return u;
-                }
-            });
-        }
+        addRenderableWidget(new Button(x, y, 20, 20, 0, 0, 20, KRYEIT_ICON, 60, 20, null, btn -> this.minecraft.setScreen(new WardrobeScreen(this))) {
+            @Override
+            public void renderWidget(GuiGraphics gui, int mouseX, int mouseY, float delta) {
+                renderVanillaBackground(gui, mouseX, mouseY, delta);
+                super.renderWidget(gui, mouseX, mouseY, delta);
+            }
+        });
     }
 }
